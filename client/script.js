2cfe210ac3690fcc38e6d5ebe21983aeadf92853
LingaiahMekala <120702468+LingaiahMekala@users.noreply.github.com>
2022-12-31 18:34:27 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('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', {
+    const response = await fetch('https://lingaiah-ai-world.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
